8bbaed4708011920e0023545a285062e35d095f0
max <mkondel@gmail.com>
2016-05-28 18:42:12 -0400

    working on having all bg layers the same img size

--- Assets/Scripts/ScrollTexture.cs
+++ Assets/Scripts/ScrollTexture.cs
@@ -5,13 +5,15 @@ public class ScrollTexture : MonoBehaviour {
 
 	public float scrollSpeed = 0.005f;
 	public float startAcceleration = 0.01f;
-	public Renderer rend;
+	private Renderer rend;
 	private float currSpeed;
+	private Vector2 texOff;
 
 	void Start() {
 		rend = GetComponent<Renderer>();
 //		rend.material.mainTextureOffset = new Vector2(rend.material.mainTextureOffset.x, Random.value);
 		currSpeed = 0.0f;
+		texOff = new Vector2 (rend.material.mainTextureOffset.x, rend.material.mainTextureOffset.y);
 	}
 
 	void Update() {
@@ -19,7 +21,7 @@ public class ScrollTexture : MonoBehaviour {
 			currSpeed += scrollSpeed * startAcceleration;
 //			Debug.Log ("currSpeed is " + currSpeed);
 		}
-		float offset = rend.material.mainTextureOffset.y + currSpeed * Time.deltaTime;
-		rend.material.mainTextureOffset = new Vector2(rend.material.mainTextureOffset.x, offset);
+		texOff.y = rend.material.mainTextureOffset.y + currSpeed * Time.deltaTime;
+		rend.material.mainTextureOffset = texOff;
 	}
 }
