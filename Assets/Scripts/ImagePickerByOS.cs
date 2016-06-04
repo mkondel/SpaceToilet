a4da035f41f8d9484272b766dc8649e3310dd512
max <mkondel@gmail.com>
2016-06-03 20:34:25 -0400

    script that resizes based on os

---
+++ Assets/Scripts/ImagePickerByOS.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ImagePickerByOS : MonoBehaviour {
+
+	public Vector3 iosDeltaPos;
+	public float iosDeltaScale;
+	public Vector3 droidDeltaPos;
+	public float droidDeltaScale;
+
+	// Use this for initialization
+	void Start () {
+		
+		RectTransform rect = GetComponent<RectTransform>();
+
+		#if UNITY_IOS
+		rect.localScale *= iosDeltaScale;
+		rect.position += iosDeltaPos;
+
+		#elif UNITY_ANDROID
+		rect.localScale *= droidDeltaScale;
+		rect.position += droidDeltaPos;
+
+		#endif
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
