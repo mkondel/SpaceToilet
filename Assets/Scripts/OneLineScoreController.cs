25f07c0734677eba0ee997ae7866503c8d600d4b
max <mkondel@gmail.com>
2016-07-27 16:28:42 -0400

    built the UI for top 10 scoreboard, started the code for that

---
+++ Assets/Scripts/OneLineScoreController.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+//OneLineScoreController class handles setting the values for one line in the top 10 scores board
+public class OneLineScoreController : MonoBehaviour {
+
+	public Text playerName;
+	public Text time;
+	public Text grade;
+	public Text accuracy;
+	public Text kills;
+
+	private string nameStr;
+
+	public string NameStr {
+		get {
+			return nameStr;
+		}
+		set {
+			nameStr = value;
+			playerName.text = value;
+		}
+	}
+
+	private string timeStr;
+
+	public string TimeStr {
+		get {
+			return timeStr;
+		}
+		set {
+			timeStr = value;
+			time.text = value;
+		}
+	}
+
+	private string gradeStr;
+
+	public string GradeStr {
+		get {
+			return gradeStr;
+		}
+		set {
+			gradeStr = value;
+			grade.text = value;
+		}
+	}
+
+	private string accuracyStr;
+
+	public string AccuracyStr {
+		get {
+			return accuracyStr;
+		}
+		set {
+			accuracyStr = value;
+			accuracy.text = value;
+		}
+	}
+
+	private string killsStr;
+
+	public string KillsStr {
+		get {
+			return killsStr;
+		}
+		set {
+			killsStr = value;
+			kills.text = value;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+//	LoadFromOneString takes a string array and sets local private vars
+	public void LoadFromOneStringArray(string[] allInOne){
+		NameStr = allInOne[0];
+		TimeStr = allInOne[1];
+		GradeStr = allInOne[2];
+		AccuracyStr = allInOne[3];
+		KillsStr = allInOne[4];
+	}
+}
---
+++ Assets/Scripts/TopTenBoardController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//TopTenBoardController class handles displaying of the top 10 scores at the end of a game
+public class TopTenBoardController : MonoBehaviour {
+
+	public OneLineScoreController[] topPlaces;
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < topPlaces.Length; i++) {
+			SetRandomScore (i);
+		}
+	}
+
+
+	void SetRandomScore(int idx){
+		string[] fakeStringArray = new string[5];
+		fakeStringArray [0] = "Fake Name "+Random.Range(1,99999).ToString();
+		fakeStringArray [1] = Random.Range(1,99).ToString()+":"+Random.Range(1,99).ToString()+":"+Random.Range(1,999).ToString()+"ms";
+		fakeStringArray [2] = "A";
+		fakeStringArray [3] = Random.Range(1,99).ToString()+"%";
+		fakeStringArray [4] = Random.Range(1,99).ToString()+"%";
+		Debug.Log (fakeStringArray.ToString ());
+		topPlaces[idx].LoadFromOneStringArray (fakeStringArray);
+	}
+}
